a55d43a88aa0a278624f88ace833fa84c315cf88
Suman <sumank450@gmail.com>
2022-05-29 02:39:16 +0530

    Added/Modify new arguments *--interactive_play *--player1_name *--player2_name *--board_size Added/Modify Validate arguments for new arguments

--- argument_parser.py
+++ argument_parser.py
@@ -21,6 +21,35 @@ def parse_args(args: List[str]):
 
     # defining arguments for parser object
     parser.add_argument(
+        "-i",
+        "--interactive_play",
+        type=bool,
+        nargs=1,
+        metavar="interactive_play",
+        default=False,
+        help="Option for interactive gameplay",
+    )
+
+    parser.add_argument(
+        "-p1_name",
+        "--player1_name",
+        type=str,
+        nargs=1,
+        metavar="player_name",
+        default=None,
+        help="Name of player 1",
+    )
+
+    parser.add_argument(
+        "-p2_name",
+        "--player2_name",
+        type=str,
+        nargs=1,
+        metavar="player_name",
+        default=None,
+        help="Name of player 1",
+    )
+    parser.add_argument(
         "-p1",
         "--player1",
         type=str,
@@ -41,33 +70,13 @@ def parse_args(args: List[str]):
     )
 
     parser.add_argument(
-        "-f",
-        "--firstplayer",
-        type=str,
+        "-b",
+        "--board_size",
+        type=int,
         nargs=1,
-        metavar="P1 or P2",
-        default=None,
-        help="Specifics First player.",
-    )
-
-    parser.add_argument(
-        "-s",
-        "--shots",
-        type=str,
-        nargs="*",
-        metavar="board_cell",
-        default=None,
-        help="All the board numbers separated by spaces will be added.",
-    )
-
-    parser.add_argument(
-        "-sf",
-        "--shotsfile",
-        type=str,
-        nargs=1,
-        metavar="shots file name",
-        default="",
-        help="The file name contains shots list",
+        metavar="number",
+        default=10,
+        help="Enter size of the board.",
     )
 
     parser.add_argument(
@@ -94,16 +103,20 @@ def validate_args(args: argument_type) -> List[str]:
        List[str] : returns a list of errors
     """
     messages = []
+    if not args.player1_name:
+        messages.append(ErrorMessageCode.MISSING_ARG_PLAYER1_NAME)
+    if not args.player2_name:
+        messages.append(ErrorMessageCode.MISSING_ARG_PLAYER2_NAME)
     if not args.player1:
         messages.append(ErrorMessageCode.MISSING_ARG_PLAYER1)
     if not args.player2:
         messages.append(ErrorMessageCode.MISSING_ARG_PLAYER2)
-    if not args.firstplayer:
-        messages.append(ErrorMessageCode.MISSING_ARG_FIRSTPLAYER)
-    elif args.firstplayer and args.firstplayer[0].upper() not in ["P1", "P2"]:
-        messages.append(ErrorMessageCode.INVALID_ARG_FIRSTPLAYER)
-    if not args.shots and not args.shotsfile:
-        messages.append(ErrorMessageCode.MISSING_ARG_SHOTS)
+    # if not args.firstplayer:
+    #     messages.append(ErrorMessageCode.MISSING_ARG_FIRSTPLAYER)
+    # elif args.firstplayer and args.firstplayer[0].upper() not in ["P1", "P2"]:
+    #     messages.append(ErrorMessageCode.INVALID_ARG_FIRSTPLAYER)
+    # if not args.shots and not args.shotsfile:
+    #     messages.append(ErrorMessageCode.MISSING_ARG_SHOTS)
     # checks player 1 file exists on path
     if args.player1 and not os.path.exists(args.player1[0]):
         messages.append(ErrorMessageCode.INVALID_FILE_NAME.format(args.player1[0]))
@@ -115,9 +128,9 @@ def validate_args(args: argument_type) -> List[str]:
     if not os.path.exists(game_config_file):
         messages.append(ErrorMessageCode.INVALID_FILE_NAME.format(game_config_file))
     # checks if shots is a file, exists on path
-    if len(args.shotsfile) == 1 and not os.path.exists(
-        args.shotsfile[0]
-    ):  # its file name
-        messages.append(ErrorMessageCode.INVALID_FILE_NAME.format(args.shotsfile[0]))
+    # if len(args.shotsfile) == 1 and not os.path.exists(
+    #     args.shotsfile[0]
+    # ):  # its file name
+    #     messages.append(ErrorMessageCode.INVALID_FILE_NAME.format(args.shotsfile[0]))
 
     return messages
